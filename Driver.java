c31ded97fb28108b45ec273f7190e76086075ba5
TanDat <ngotandatntd2804@gmail.com>
2025-09-28 19:16:58 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,14 +1,50 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Driver {
-	public static void main(String[]args) {
-		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
-		double[] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double[] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
-		Polynomial s = p1.add(p2);
-		System.out.println("s(0.1)=" + s.evaluate(0.1));
-		if (s.hasRoot(1)) System.out.println("1 is a root of s");
-		else System.out.println("1 is not a root of s");
+	public static void main(String[]args) throws IOException {
+		// add
+		double[] c1 = new double[] {2, 3};
+		int[] e1 = new int[] {0, 1};
+		Polynomial p1 = new Polynomial(c1, e1);
+		double[] c2 = new double[] {4, 5};
+		int[] e2 = new int[] {1, 2};
+		Polynomial p2 = new Polynomial(c2, e2);
+		Polynomial p3 = p1.add(p2);
+		System.out.println("This is polynomial p3 evaluated at x = 2");
+		System.out.println(p3.evaluate(2)); // expect 36
+		
+		double[] ca = new double[] {2, 3};
+		int[] ea = new int[] {1, 2};
+		double[] cb = new double[] {-2, 5};
+		int[] eb = new int[] {1, 3};
+ 		Polynomial a = new Polynomial(ca, ea);
+		Polynomial b = new Polynomial(cb, eb);
+		Polynomial c = a.add(b);
+		System.out.println("This is polynomial c evaluated at x = 3");
+		System.out.println(c.evaluate(3)); // expect 162
+		
+		// multiply
+		double[] c4 = new double[] {2, 3};
+		int[] e4 = new int[] {0, 1};
+		Polynomial p4 = new Polynomial(c4, e4);
+		double[] c5 = new double[] {4, 5};
+		int[] e5 = new int[] {1, 2};
+		Polynomial p5 = new Polynomial(c5, e5);
+		Polynomial p6 = p4.multiply(p5);
+		System.out.println("This is polynomial p6 evaluated at x = 2");
+		System.out.println(p6.evaluate(2)); //expect 224
+		
+		// Polynomial takes file
+		File f = new File("/Users/ngotandat/test_poly/poly_take.txt");
+		Polynomial p7 = new Polynomial(f);
+		System.out.println("This is polynomial p7 evaluated at x = 2");
+		System.out.println(p7.evaluate(2)); //expected 1785
+		
+		// saveToFile
+		double[] c8 = new double[] {3, -2, 5, -1};
+		int[] e8 = new int[] {0, 1, 2, 3};
+		Polynomial p8 = new Polynomial(c8, e8);
+		p8.saveToFile("/Users/ngotandat/test_poly/poly_receive.txt"); // expect "3-2x1+5x2-1x3" poly_receive
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,52 +1,147 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Polynomial {
-	// field of coefficients
+	// field of non-zero coefficients
 	double[]coeff;
+	// field of exponents
+	int[]exp;
 	// constructor takes no argument
 	public Polynomial() {
-		coeff = new double[] {0};
+		coeff = new double[0];
+		exp = new int[0];
 	}
-	// constructor takes an array of double
-	public Polynomial(double[]array) {
-		coeff = new double[array.length];
-		for (int i=0; i < array.length; i++) {
-			coeff[i] = array[i];
+	// constructor takes an array of coefficients and an array of exponents
+	public Polynomial(double[]c, int[]e) {
+		this.coeff = c;
+		this.exp = e;
+	}
+	// constructor takes a file
+	public Polynomial(File f) throws IOException {
+		// parseInt, parseDouble, split
+		BufferedReader reader = new BufferedReader(new FileReader(f));
+		String p = reader.readLine();
+		String regex = "(?=[+-])"; //split just before +/-, ?= positve lookahead
+		String[] strArray = p.split(regex);
+		// For testing purpose
+		/*for (String s:strArray) {
+			System.out.println(s);
+		}*/
+		// parseDouble for coeff., parseInt for exp.
+		this.coeff = new double[strArray.length];
+		this.exp = new int[strArray.length];
+		for (int i=0; i<strArray.length; i++) {
+			// For constant
+			if (strArray[i].length() == 1) {
+				this.coeff[i] = Double.parseDouble(strArray[i]);
+			}
+			// For coefficients and exponents
+			else {
+				String[] both = strArray[i].split("[x]"); //split the coefficent and the exponent
+				this.coeff[i] = Double.parseDouble(both[0]); // 1st element is coefficient
+				this.exp[i] = Integer.parseInt(both[1]); // 2nd element is exponent
+			}
 		}
 	}
 	// add method
 	Polynomial add(Polynomial p) {
-		Polynomial res = new Polynomial();
-		// choose which array has the greatest length and setup in res
-		if (this.coeff.length > p.coeff.length){
-			res.coeff = new double[this.coeff.length];
-			for (int k=0; k < this.coeff.length; k++) {
-				res.coeff[k] = this.coeff[k];
+		// Hashmap of (exp,coeff)
+		HashMap<Integer, Double> res = new HashMap<>();
+		// insert all coeff and exp of the first polynomial
+		for (int i =0; i<this.coeff.length; i++) {
+			res.put(this.exp[i], this.coeff[i]);
+		}
+		// insert all coeff and exp of the second polynomial
+		// if exp exists, add the new and old coeff instead
+		for (int j=0; j<p.coeff.length; j++) {
+			if (res.containsKey(p.exp[j])) {
+				// if the sum of coeffs is non-zero, add
+				if (p.coeff[j] + res.get(p.exp[j]) != 0.0) {
+					res.replace(p.exp[j], res.get(p.exp[j]) + p.coeff[j]);
+				}
+				// if zero, remove
+				else res.remove(p.exp[j]);
 			}
-			// add the elements of the remaining array to res
-			for (int i=0; i < p.coeff.length; i++) {
-				res.coeff[i] += p.coeff[i];
+			else res.put(p.exp[j], p.coeff[j]);
+		}
+		// put back to array
+		double[] coeff = new double[res.size()];
+		int[] exp = new int[res.size()];
+		int k = 0;
+		for (Integer e:res.keySet()) {
+			coeff[k] = res.get(e);
+			exp[k] = e;
+			k++;
+		}
+		return new Polynomial(coeff, exp);
+	}
+	// multiply method
+	Polynomial multiply(Polynomial p) {
+		// Hasmap of (exp,coeff)
+		HashMap <Integer, Double> res = new HashMap<>();
+		// for every coeff of this, multiply it with each coeff of p
+		// for every exp of this, add it with each exp of p
+		for (int i=0; i<p.coeff.length; i++) {
+			for (int j=0; j<this.coeff.length; j++) {
+				Integer exp = this.exp[j] + p.exp[i];
+				if (res.containsKey(exp)) {
+					res.replace(exp, res.get(exp) + (this.coeff[j] * p.coeff[i]));
+				}
+				else res.put(exp, this.coeff[j] * p.coeff[i]);
 			}
 		}
-		else {
-			res.coeff = new double[p.coeff.length];
-			for (int k=0; k < p.coeff.length; k++) {
-				res.coeff[k]= p.coeff[k];
-			}
-			for (int i=0; i < this.coeff.length; i++) {
-				res.coeff[i] += this.coeff[i];
-			}
+		// put back to array
+		double[] coeff = new double[res.size()];
+		int[] exp = new int[res.size()];
+		int k = 0;
+		for (Integer e:res.keySet()) {
+			coeff[k] = res.get(e);
+			exp[k] = e;
+			k++;
 		}
-		return res;
+		return new Polynomial(coeff, exp);
 	}
 	// evaluate method
 	double evaluate(double x) {
 		double result = 0.0;
 		for (int i=0; i < this.coeff.length; i++) {
-			result += (this.coeff[i] * Math.pow(x, i));
+			result += (this.coeff[i] * Math.pow(x, this.exp[i]));
 		}
 		return result;
 	}
 	// hasRoot method
 	boolean hasRoot(double r) {
-		return this.evaluate(r) == 0;
+		return this.evaluate(r)==0.0;
+	}
+	// saveToFile method
+	void saveToFile(String fname) throws IOException {
+		// Convert the polynomial in the file to a string text like "5-3x2+7x8"
+		String poly = "";
+		// add the constant of the polynomial first (if have)
+		if (this.exp[0]==0) {
+			poly = poly.concat(String.valueOf((int)this.coeff[0]));
+		}
+		// format: "coeff" + "x" + "exp"
+		for (int i=1; i<this.coeff.length; i++) {
+			String coeff = String.valueOf((int)this.coeff[i]);
+			String exp = String.valueOf(this.exp[i]);
+			if (this.coeff[i]>0) {
+				String term = "+" + coeff + "x" + exp;
+				poly = poly.concat(term);
+			}
+			else {
+				String term = coeff + "x" + exp;
+				poly = poly.concat(term);
+			}
+		}
+		BufferedWriter text = new BufferedWriter(new FileWriter(fname));
+		text.write(poly);
+		text.close();
 	}
 }
